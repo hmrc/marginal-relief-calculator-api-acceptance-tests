62fb29829dcad1c6a17c37b762b07e4efe4c08d8
sivathota226 <108057130+sivathota226@users.noreply.github.com>
2022-06-29 16:41:41 +0100

    MRC-92 API Automation feature file-Improvement

--- run_marginal_relief_calculator_tests.sh
+++ run_marginal_relief_calculator_tests.sh
@@ -15,7 +15,7 @@ echo "*** running on $environment for tags '$tags' ***"
 status_code=$(curl --write-out %{http_code} --silent --output /dev/null http://localhost:7100/ping/ping)
 if [[ "$status_code" -ne 200 ]] ; then
   echo "ping endpoint response error $status_code.
-  Time to pay Eligibility does not look like it is running.
+  MRC Service does not look like it is running.
   No tests will be executed. Build failure!"
   exit 1
 fi

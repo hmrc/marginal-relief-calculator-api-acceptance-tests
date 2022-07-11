08f14b2eadf54df96a581758531b66b5b96061e8
sara-raj <100203074+Sara-raj@users.noreply.github.com>
2022-07-11 16:11:22 +0100

    introduced sleep

--- run_marginal_relief_calculator_tests.sh
+++ run_marginal_relief_calculator_tests.sh
@@ -9,7 +9,7 @@ if [ $# -gt 0 -a "$1" != "$environment" ]; then
   tags="not @wip and not @ignore"
 fi
 echo "*** running on $environment for tags '$tags' ***"
-
+sleep 20
 status_code=$(curl --write-out %{http_code} --silent --output /dev/null http://localhost:7100/ping/ping)
 if [[ "$status_code" -ne 200 ]]; then
   echo "ping endpoint response error $status_code.

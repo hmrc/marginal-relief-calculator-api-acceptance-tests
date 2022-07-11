f965f7845837f0e9280a45f42dcac43ea03485a2
sara-raj <100203074+Sara-raj@users.noreply.github.com>
2022-07-11 11:56:08 +0100

    jenkins failure fix

--- run_marginal_relief_calculator_tests.sh
+++ run_marginal_relief_calculator_tests.sh
@@ -9,23 +9,7 @@ if [ $# -gt 0 -a "$1" != "$environment" ]; then
   tags="not @wip and not @ignore"
 fi
 echo "*** running on $environment for tags '$tags' ***"
-#sm --start MARGINAL_RELIEF_CALCULATOR_BACKEND --appendArgs '{"MARGINAL_RELIEF_CALCULATOR_BACKEND": [
-#"-Dcalculator-config.fy-configs.0.year=2022",
-#"-Dcalculator-config.fy-configs.0.main-rate=0.19",
-#"-Dcalculator-config.fy-configs.1.year=2023",
-#"-Dcalculator-config.fy-configs.1.lower-threshold=50000",
-#"-Dcalculator-config.fy-configs.1.upper-threshold=250000",
-#"-Dcalculator-config.fy-configs.1.small-profit-rate=0.19",
-#"-Dcalculator-config.fy-configs.1.main-rate=0.25",
-#"-Dcalculator-config.fy-configs.1.marginal-relief-fraction=0.015",
-#"-Dcalculator-config.fy-configs.2.year=2024",
-#"-Dcalculator-config.fy-configs.2.lower-threshold= 50000",
-#"-Dcalculator-config.fy-configs.2.upper-threshold= 300000",
-#"-Dcalculator-config.fy-configs.2.small-profit-rate=0.19",
-#"-Dcalculator-config.fy-configs.2.main-rate=0.25",
-#"-Dcalculator-config.fy-configs.2.marginal-relief-fraction=0.015"]}'
-#sleep 8
-#echo "Thanks for waiting"
+
 status_code=$(curl --write-out %{http_code} --silent --output /dev/null http://localhost:7100/ping/ping)
 if [[ "$status_code" -ne 200 ]]; then
   echo "ping endpoint response error $status_code.
